fcdc4baa461d41a6174e4ca4c3b008d8ac70d072
developer-unknown <cardenashernandezmarcos14@gmail.com>
2025-10-18 16:33:29 -0600

    Configuracion de API y BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { PostsModule } from './posts/posts.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PostsModule,
+    
+    CommonModule,
+
+    ConfigModule.forRoot(),
+    
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    
+    SeedModule,
+    
+  ],
   controllers: [],
   providers: [],
 })
